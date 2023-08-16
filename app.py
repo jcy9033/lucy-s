ace7874b29be6e132a67247bf55670981a604245
jcy9033 <62980040+jcy9033@users.noreply.github.com>
2023-08-16 11:22:47 +0900

    Query failed.

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+@app.route ('/')
+def home():
+    return 'Hello World!\n'
+
+app.config['MYSQL_HOST'] = '10.0.1.4'
+app.config['MYSQL_USER'] = 'azureuser'
+app.config['MYSQL_PASSWORD'] = '1q2w3e4r####'
+app.config['MYSQL_DB'] = 'lucys'
+
+mysql = MySQL(app)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True) # Debug mode enabled
+
+@app.route ('/reservations')
+def reservations():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM reservationsTable")
+    data = cur.fetchall()
+    return str(data)
+
+# @app.route('/reservations')
+# def reservations():
+#     cur = mysql.connection.cursor()
+#     cur.execute("SELECT * FROM reservationsTable")
+#     data = cur.fetchall()
+#     result = '\n'.join(map(str, data))
+#     return result

7e7e4be39d55e4e7bd96dc1e1828372dabb9d4be
jcy9033 <62980040+jcy9033@users.noreply.github.com>
2023-08-16 13:24:13 +0900

    Before POST Function.

--- app.py
+++ app.py
@@ -2,17 +2,17 @@ from flask import Flask
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-
-@app.route ('/')
-def home():
-    return 'Hello World!\n'
-
 app.config['MYSQL_HOST'] = '10.0.1.4'
 app.config['MYSQL_USER'] = 'azureuser'
 app.config['MYSQL_PASSWORD'] = '1q2w3e4r####'
 app.config['MYSQL_DB'] = 'lucys'
 
 mysql = MySQL(app)
+
+@app.route ('/')
+def home():
+    return 'Hello World!' + '\n'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True) # Debug mode enabled
